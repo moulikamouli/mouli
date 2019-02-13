2a6b3f8a28a362306efa0d48aca301d47f2730d0
moulikamouli <47565377+moulikamouli@users.noreply.github.com>
2019-02-13 21:38:38 +0530

    Create Hello.c

---
+++ Hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+   int number,i;
+   clrscr();
+   printf("to print hello word N times");
+   printf("Enter a number:");
+   scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{ 
+		printf("Hello");
+	}
+	return 0;
+	getch();
+}

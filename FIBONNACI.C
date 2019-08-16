16de3748b18b901f5e80839f6091146db84c4ca0
moulikamouli <47565377+moulikamouli@users.noreply.github.com>
2019-08-16 07:02:28 +0530

    Create FIBONNACI.C

---
+++ FIBONNACI.C
@@ -0,0 +1,14 @@
+void main()
+{
+int n=0,m=1,s,i,num;
+printf("enter the elements");
+scanf("%d",&num);
+printf("%d%d",&,n,m);
+for(i=2;i<num;++i)
+{
+s=n+m;
+printf("%d",s);
+n=m;
+m=s;
+}
+getch();
